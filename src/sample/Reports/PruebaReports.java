bc3f70625c3d1a796584ad87af85c7f5f9644a47
axellong <193250@ids.upchiapas.edu.mx>
2021-04-19 00:41:21 -0500

    report cobrar and pay are completed, is missing tha class full them

--- src/sample/Reports/PruebaReports.java
+++ src/sample/Reports/PruebaReports.java
@@ -5,18 +5,25 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
-import sample.Main;
-import sample.Reports.ReportsIndicadores;
 
 import javax.swing.*;
 
 public class PruebaReports {
     public PruebaReports(){
-        try {            JasperReport jasper = (JasperReport) JRLoader.loadObject(getClass().getResource("/sample/Reports/Jasper/Reporte.jasper"));
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasper,null, ReportsIndicadores.getDataSource());
-            JasperViewer viewer =  new JasperViewer(jasperPrint, false);
-            viewer.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            viewer.setVisible(true);
+        try {
+//            System.out.println("entro");
+//            JasperReport jasper = (JasperReport) JRLoader.loadObject(getClass().getResource("/sample/Reports/Jasper/ReportePagar.jasper"));
+//            JasperPrint jasperPrint = JasperFillManager.fillReport(jasper,null, ReportsCuentasPagar.getDataSource());
+//            JasperViewer viewer =  new JasperViewer(jasperPrint, false);
+//            viewer.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+//            viewer.setVisible(true);
+
+            JasperReport jas = (JasperReport) JRLoader.loadObject(getClass().getResource("/sample/Reports/Jasper/ReportePagar.jasper"));
+            JasperPrint jsp = JasperFillManager.fillReport(jas,null, ReportsCuentasPagar.getDataSource());
+            JasperViewer vie =  new JasperViewer(jsp, false);
+            vie.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            vie.setVisible(true);
+            System.out.println("salio");
         }catch (Exception exception){
             exception.getMessage();
         }
